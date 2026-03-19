0270814899f7616cc515212537c3ad9f61c92efc
eugeneakeh <aakaay77@gmail.com>
2026-03-19 23:22:17 +0100

    Update dashboard.py

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import plotly.graph_objects as go
-from core_engine import run_all_engines
-
+from engines.core_engine import run_all_engines
 
 def show():
     st.header("Strategic Intelligence Dashboard")
