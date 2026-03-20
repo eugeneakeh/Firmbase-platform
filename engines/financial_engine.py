5575e7f9e59866a6de84a443a8a56cddd1c6b22f
eugeneakeh <aakaay77@gmail.com>
2026-03-20 05:59:47 +0100

    Update financial_engine.py

--- engines/financial_engine.py
+++ engines/financial_engine.py
@@ -1,16 +1,1 @@
-def run_financial_engine(starting_cash, commitments, revenue, cost, risk):
 
-    cash_available = starting_cash - commitments
-
-    roi = (revenue - cost) / cost
-
-    risk_adjusted_roi = roi * (1 - risk)
-
-    projected_cashflow = cash_available + revenue - cost
-
-    return {
-        "cash_available": cash_available,
-        "roi": roi,
-        "risk_adjusted_roi": risk_adjusted_roi,
-        "projected_cashflow": projected_cashflow
-    }
