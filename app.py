06f620c9cf934e6b49e1b7af5425bd5d07f392d5
eugeneakeh <aakaay77@gmail.com>
2026-03-20 06:35:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
+import streamlit as st
+from pages.dashboard import show
 
+
+# -----------------------------
+# PAGE CONFIG
+# -----------------------------
+st.set_page_config(
+    page_title="Firmbase",
+    page_icon="📊",
+    layout="wide"
+)
+
+
+# -----------------------------
+# APP ENTRY
+# -----------------------------
+def main():
+    show()
+
+
+if __name__ == "__main__":
+    main()
