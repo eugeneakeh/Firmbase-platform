e746314a7b9da15b643e3c988779d4afcd97c8ad
eugeneakeh <aakaay77@gmail.com>
2026-03-24 19:25:22 +0100

    Update dashboard.py

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 
 from engines.business_case_engine import BusinessCaseEngine
 from core.central_engine import run_central_engine
